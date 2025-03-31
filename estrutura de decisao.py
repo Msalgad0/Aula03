6217984f2994e702471128a4a0edd3915b5376c3
Matheus <matheus0509@live.com>
2025-03-31 16:14:36 -0300

    Estrutura Decisão

---
+++ estrutura de decisao.py
@@ -0,0 +1,9 @@
+# if, else, if else
+
+numero1 = int(input("Digite um número:\n"))
+numero2 = int(input("Digite um número: \n"))
+
+if numero1 > numero2 :
+    print(f"{numero1} é maior\n")
+else :
+    print(f"{numero2} é maior\n")
