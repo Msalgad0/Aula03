20ffc509cf474899557b3957eb5819913e12417c
Matheus <matheus0509@live.com>
2025-03-31 15:10:51 -0300

    Atualizando codigo

---
+++ exercicio03.py
@@ -0,0 +1,13 @@
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite um numero: "))
+
+soma = numero1 + numero2
+subtração = numero1 - numero2
+divisao = numero1 / numero2
+multiplicação = numero1 * numero2
+
+print("Vamos ver as 4 operações com esses numero\n",
+      f"Soma:{soma}\n",
+      f"Subtração:{subtração}\n",
+      f"Divisão:{divisao}\n",
+      f"Multiplicação:{multiplicação}\n")
